89080057a964db0863601d4076a96df7eeeaef8f
lordtt13 <thakurtanmay72@yahoo.com>
2020-06-11 16:25:40 +0530

    Simple Handler func add
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- intro-app/src/App.js
+++ intro-app/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class app extends Component {
+    constructor(props) {
+        super (props)
+
+        this.state = {count:0}
+    }
+    
+    handleClick = () => {
+        this.setState({count: this.state.count + 1})
+    }
+    
+  render() {
+    let inst = 'VIT'
+    let {count} = this.state;
+    return (
+        <div>
+          <h1>It's been {count} days since I've been back to {inst}</h1>
+            <span>
+            <button onClick = {() => this.handleClick()}> Click here to increment count </button>
+            </span>
+        </div>
+    )
+  }
 }
 
-export default App;
+export default app;
