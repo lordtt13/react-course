fa667f40ee5528c3d7607192c153112e4b49adf5
lordtt13 <thakurtanmay72@yahoo.com>
2020-06-11 16:43:15 +0530

    Handler logic updated
    Signed-off-by: lordtt13 <thakurtanmay72@yahoo.com>

--- intro-app/src/App.js
+++ intro-app/src/App.js
@@ -4,19 +4,29 @@ class app extends Component {
     constructor(props) {
         super (props)
 
-        this.state = {count:0}
+        this.state = {count:0, over : false}
     }
     
     handleClick = () => {
         this.setState({count: this.state.count + 1})
     }
-    
-  render() {
+
+    componentDidUpdate(props, state) {
+        if(this.state.count > 10 && this.state.count != state.count && !this.state.over){
+            this.state.over = true
+        }
+    }
+
+    render() {
     let inst = 'VIT'
     let {count} = this.state;
     return (
         <div>
           <h1>It's been {count} days since I've been back to {inst}</h1>
+            {this.state.over?
+                <h3>Beat the high score here!</h3>
+                :null
+            }
             <span>
             <button onClick = {() => this.handleClick()}> Click here to increment count </button>
             </span>
